d85ef71870cdb327d6f2fad28f41d9d63b62d11d
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-26 18:31:36 +0100

    style: add Container and Header, use next/image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import { Container, Header } from '../styles/pages/app'
 import { globalStyles } from '../styles/global'
+import LogoImg from '../assets/logo.svg'
 
 globalStyles()
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImg} alt="Ignite Shop Logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
